09bef6c0b8f4f3a9a2413c025d2a6071472ac30b
koszkota <koszkota@gmail.com>
2018-11-12 17:00:32 +0000

    Increase cov: add missing test to game.

--- game/game_test.go
+++ game/game_test.go
@@ -9,7 +9,7 @@ import (
 	"tictactoe/testhelper"
 )
 
-func TestGamePlaysWholeHumanvsHumanTieGame(t *testing.T) {
+func TestGamePlaysWholeHumanVsHumanTieGame(t *testing.T) {
 	movesOfPlayers := "5\n1\n3\n7\n4\n6\n2\n8\n9\n"
 	stubWriter := new(clui.StubWriter)
 	clui := clui.MakeClui(strings.NewReader(movesOfPlayers), stubWriter)
@@ -25,7 +25,7 @@ func TestGamePlaysWholeHumanvsHumanTieGame(t *testing.T) {
 	stubWriter.CleanOutputs()
 }
 
-func TestGamePlaysWholeHumanvsHumanWonGame(t *testing.T) {
+func TestGamePlaysWholeHumanVsHumanWonGameForPlayerOne(t *testing.T) {
 	movesOfPlayers := "1\n2\n4\n5\n7\n"
 	stubWriter := new(clui.StubWriter)
 	clui := clui.MakeClui(strings.NewReader(movesOfPlayers), stubWriter)
@@ -37,6 +37,22 @@ func TestGamePlaysWholeHumanvsHumanWonGame(t *testing.T) {
 	aGame.Play()
 
 	expectedFinalMessage :=  "Player X won!"
+	matchers.EqualLiterals(t, expectedFinalMessage, stubWriter.GetLastMessage())
+	stubWriter.CleanOutputs()
+}
+
+func TestGamePlaysWholeHumanVsHumanWonGameForPlayerTwo(t *testing.T) {
+	movesOfPlayers := "1\n2\n3\n5\n6\n8\n"
+	stubWriter := new(clui.StubWriter)
+	clui := clui.MakeClui(strings.NewReader(movesOfPlayers), stubWriter)
+	board := board.MakeBoard(3)
+	playerOne := player.Human{Mark: "X", Clui: clui}
+	playerTwo := player.Human{Mark: "Y", Clui: clui}
+	aGame := MakeGame(clui, &board, playerOne, playerTwo)
+
+	aGame.Play()
+
+	expectedFinalMessage :=  "Player Y won!"
 	matchers.EqualLiterals(t, expectedFinalMessage, stubWriter.GetLastMessage())
 	stubWriter.CleanOutputs()
 }
