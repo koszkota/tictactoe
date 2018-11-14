262eef7ecc716b5416e55777b9b50af62c53c009
koszkota <koszkota@gmail.com>
2018-11-14 14:54:48 +0000

    Test: add missing test to gamescontroller.

--- gamescontroller/gamescontroller_test.go
+++ gamescontroller/gamescontroller_test.go
@@ -20,6 +20,18 @@ func TestRunsAComputerVsComputerGameAndEndsWhenRunStatusIsFalse(t *testing.T) {
 	matchers.EqualLiterals(t, "It's a tie!", stubWriter.GetLastMessage())
 }
 
+func TestOnInvalidInputInMainMenuNeedsToSubmitAgain(t *testing.T) {
+	stubWriter := &clui.StubWriter{}
+	aClui := clui.MakeClui(strings.NewReader("invalid\nYES\n3\nNO"), stubWriter)
+	stubRunStatus := &StubRunStatus{Counter: 0}
+	gameFactory := &game.GameFactory{Clui:aClui}
+	gamesController := GamesController{Clui:aClui, RunStatus: stubRunStatus, GameFactory:gameFactory}
+
+	gamesController.Run()
+
+	matchers.EqualLiterals(t, "It's a tie!", stubWriter.GetLastMessage())
+}
+
 func TestRunsAHumanVsComputerGameAndEndsWhenRunStatusIsFalse(t *testing.T) {
 	stubWriter := &clui.StubWriter{}
 	aClui := clui.MakeClui(strings.NewReader("YES\n2\nH\n1\n2\n6\nNO"), stubWriter)
