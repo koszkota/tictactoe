b67c2d81b253e8268c28fa7c0138cf0a7df9b563
koszkota <koszkota@gmail.com>
2018-11-14 15:32:45 +0000

    Refactor: refactor methods in gamefactory.

--- game/gamefactory.go
+++ game/gamefactory.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"strings"
 	"tictactoe/board"
 	"tictactoe/clui"
 	"tictactoe/player"
@@ -14,44 +15,60 @@ func (gameFactory *GameFactory) CreateGame() *Game {
 	gameFactory.Clui.AskForGameMode()
 	pick := gameFactory.Clui.ReadUserInput()
 	if pick == "1" {
-		playerOne := player.Human{Mark: "X", Clui:gameFactory.Clui}
-		playerTwo := player.Human{Mark: "Y", Clui:gameFactory.Clui}
-		marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
-		aBoard := board.MakeBoard(3, &marksRepo)
-		aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
-		return &aGame
+		return gameFactory.getHumanVsHumanGame()
 	} else if pick == "2" {
-		return gameFactory.whoGoesFirst()
+		return gameFactory.getMixedPlayersGame()
 	} else if pick == "3" {
-		playerOne := player.Computer{Mark: "X"}
-		playerTwo := player.Computer{Mark: "Y"}
-		marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
-		aBoard := board.MakeBoard(3, &marksRepo)
-		aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
-		return &aGame
+		return gameFactory.getComputerVsComputerGame()
 	} else {
 		return gameFactory.CreateGame()
 	}
 }
 
-func (gameFactory *GameFactory) whoGoesFirst() *Game {
+func (gameFactory *GameFactory) getMixedPlayersGame() *Game {
 	gameFactory.Clui.AskWhoGoesFirst()
 	pick := gameFactory.Clui.ReadUserInput()
-	if pick == "H" {
-		playerOne := player.Human{Mark: "X", Clui:gameFactory.Clui}
-		playerTwo := player.Computer{Mark: "Y"}
-		marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
-		aBoard := board.MakeBoard(3, &marksRepo)
-		aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
-		return &aGame
-	} else if pick == "C" {
-		playerOne := player.Computer{Mark: "X"}
-		playerTwo := player.Human{Mark: "Y", Clui:gameFactory.Clui}
-		marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
-		aBoard := board.MakeBoard(3, &marksRepo)
-		aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
-		return &aGame
+	if strings.EqualFold(pick, "h") {
+		return gameFactory.getHumanVsComputerGame()
+	} else if strings.EqualFold(pick, "c") {
+	return gameFactory.getComputerVsHumanGame()
 	} else {
-		return gameFactory.whoGoesFirst()
+		return gameFactory.getMixedPlayersGame()
 	}
+}
+
+func (gameFactory *GameFactory) getHumanVsHumanGame() *Game {
+	playerOne := player.Human{Mark: "X", Clui:gameFactory.Clui}
+	playerTwo := player.Human{Mark: "Y", Clui:gameFactory.Clui}
+	marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
+	aBoard := board.MakeBoard(3, &marksRepo)
+	aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
+	return &aGame
+}
+
+func (gameFactory *GameFactory) getComputerVsComputerGame() *Game {
+	playerOne := player.Computer{Mark: "X"}
+	playerTwo := player.Computer{Mark: "Y"}
+	marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
+	aBoard := board.MakeBoard(3, &marksRepo)
+	aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
+	return &aGame
+}
+
+func (gameFactory *GameFactory) getHumanVsComputerGame() *Game {
+	playerOne := player.Human{Mark: "X", Clui: gameFactory.Clui}
+	playerTwo := player.Computer{Mark: "Y"}
+	marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
+	aBoard := board.MakeBoard(3, &marksRepo)
+	aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
+	return &aGame
+}
+
+func (gameFactory *GameFactory) getComputerVsHumanGame() *Game {
+	playerOne := player.Computer{Mark: "X"}
+	playerTwo := player.Human{Mark: "Y", Clui:gameFactory.Clui}
+	marksRepo := board.MarksRepo{PlayerOneMark: playerOne.GetMark(), PlayerTwoMark: playerTwo.GetMark()}
+	aBoard := board.MakeBoard(3, &marksRepo)
+	aGame := MakeGame(gameFactory.Clui, &aBoard, playerOne, playerTwo)
+	return &aGame
 }
